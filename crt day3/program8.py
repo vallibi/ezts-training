35e03f12383c1f1f0639e0fb9d5c2b773227b64a
vallibi <160836822+vallibi@users.noreply.github.com>
2024-02-22 19:33:50 +0530

    Update program8.py

--- crt day3/program8.py
+++ crt day3/program8.py
@@ -1,4 +1,4 @@
-#write a python program to print sum of n natural no.using recursio
+#write a python program to print sum of n natural no.using recursion
 def printing(n):
     if n < 1:
         return 1              #input=10 or any value,output for 10=10 9 8 7 6 5 4 3 2 1 ,sum=56
