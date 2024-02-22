54765198de240b827da63028d7ab6512b4296bad
vallibi <160836822+vallibi@users.noreply.github.com>
2024-02-22 19:42:26 +0530

    Delete program9.py

--- program9.py
+++
@@ -1,9 +0,0 @@
-#write a python program whether the string is panagram or not (panagram=it contains all alphabets means A to z of alphabets )
-import string
-s=input()
-s=s.lower()
-s1=string.ascii_lowercase
-if set(s)==set(s1):
-    print("panagram")
-else:
-    print("not a panagram")
